9037d7523d763ecbb0f04cdd3dba284aecb3ea62
Josito <josalbarr@alu.edu.gva.es>
2024-04-22 17:17:36 +0200

    Seguir con el 6

--- JavaScriptXJSON/dun/Exercicis/script.js
+++ JavaScriptXJSON/dun/Exercicis/script.js
@@ -2,13 +2,13 @@
 let fondoVerdeTitulo = document.getElementById('titol').style.background = "#006400";
 
 // Añadir nueva sección al menu
-let a = document.createElement("a");
-let li = document.createElement("li");
+let Ca = document.createElement("a");
+let Cli = document.createElement("li");
 const text = document.createTextNode("Sobre nosaltres");
-li.appendChild(a);
-a.appendChild(text);
-a.href = "https://www.youtube.com/watch?v=jNY_wLukVW0&list=PLxzSZG7g8c8x6GYz_FcNr-3zPQ7npP6WF&ab_channel=Radiohead-Topic"
-document.getElementById("menu").appendChild(li, a);
+Cli.appendChild(Ca);
+Ca.appendChild(text);
+Ca.href = "https://www.youtube.com/watch?v=jNY_wLukVW0&list=PLxzSZG7g8c8x6GYz_FcNr-3zPQ7npP6WF&ab_channel=Radiohead-Topic"
+document.getElementById("menu").appendChild(Cli, Ca);
 
 // Cambiar el color de los articulos a gris claro
 let articleSelector = document.querySelectorAll('article');
@@ -17,4 +17,16 @@ for (let i = 0; i < articleSelector.length; i++) {
 }
 
 // Letra del menu en rojo
-let menuC = document.getElementById('menu').style.color = "#006400";
+let menuRojo = document.querySelectorAll("#menu li a");
+for (let i = 0; i < menuRojo.length; i++) {
+    menuRojo[i].style.color = "red";
+}
+
+// Crear un paragrafo que ponga 'Segueix-nos a les xarxes socials!' al final del main
+let p = document.createElement("p");
+let textP1 = document.createTextNode("Segueix-nos a les xarxes socials!");
+p.appendChild(textP1);
+document.getElementsByTagName('main')[0].appendChild(p);
+
+// Eliminar 'Serveis' del menu
+let menu = document.getElementById("menu");
