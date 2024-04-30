24c9adc76cb15dd3f545c56c7ab314ecf45b09cb
Josito <josalbarr@alu.edu.gva.es>
2024-04-30 20:29:51 +0200

    cringe

--- Proyecto/scriptProyect/script.js
+++ Proyecto/scriptProyect/script.js
@@ -23,11 +23,21 @@ Ca.appendChild(text);
 Ca.href = "https://youtu.be/d1WsxRc2X04"
 title.appendChild(Ca);
 
-// 3. Cambiar fondo cuando pongas el cursor encima de algo
+// 3. Cambiar fondo cuando pongas el cursor encima de algo (en la página de KH)
+let roxas = document.getElementById("roxas");
+let sora = document.getElementById("sora");
+
+roxas.addEventListener('mouseenter', function() {
+    document.body.style.backgroundImage = "url('https://miro.medium.com/v2/resize:fit:1400/1*sK-oygRJt6uz5mHjk_bYSQ.jpeg')"; 
+});
 
 
+// 4. Hazle click a Sora y te vas a mi página de Majima by de face ;3
+sora.addEventListener('click', function() {
+  window.location.href = 'https://arthurmorganroadthe1899.github.io';
+});
 
-
+// 5. 
 
 
 
